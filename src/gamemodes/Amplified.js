4107595dd88ef0aa0a75d65d3c73eea40496f93b
AmaZiX <amazichannel@gmail.com>
2015-08-21 09:57:56 +0700

    Setting base code

--- src/gamemodes/Amplified.js
+++ src/gamemodes/Amplified.js
@@ -1,1 +1,9 @@
+var Mode = require('./Mode');
 
+function Amplified() {
+    Mode.apply(this, Array.prototype.slice.call(arguments));
+
+    this.ID = 22;
+    this.name = "Amplified";
+    this.specByLeaderboard = true;
+}
